9453d350266541fc65970c594cd8e9837712402a
victor-floriano <victorgf@bu.edu>
2024-10-09 12:13:12 +0000

    Added loading step to the flow script and removed old flow files

--- sandbox/test-victor/extract-txt-and-transform/test-flow/etl-flow.py
+++ sandbox/test-victor/extract-txt-and-transform/test-flow/etl-flow.py
@@ -28,10 +28,21 @@ def transform_txt_to_dataframe():
     cloud_function_url = "https://transform-txt-to-dataframe-162771833878.us-central1.run.app"
     response = requests.post(cloud_function_url)
     if response.status_code == 200:
-        print("Upload cloud function executed successfully: ", response.text)
+        print("Transformation cloud function executed successfully: ", response.text)
     else:
-        print(f"Failed to execute upload cloud function. Status code: {response.status_code}, Response: {response.text}")
-        raise Exception("Data upload failed")
+        print(f"Failed to execute transformation cloud function. Status code: {response.status_code}, Response: {response.text}")
+        raise Exception("Data transformation failed")
+
+# Task to call the new function that loads Parquet data into BigQuery
+@task(retries=3, retry_delay_seconds=60)
+def load_to_bigquery():
+    cloud_function_url = "https://load-to-bigquery-162771833878.us-central1.run.app"
+    response = requests.post(cloud_function_url)
+    if response.status_code == 200:
+        print("Data loaded to BigQuery successfully: ", response.text)
+    else:
+        print(f"Failed to load data into BigQuery. Status code: {response.status_code}, Response: {response.text}")
+        raise Exception("Data load to BigQuery failed")
 
 # Define the combined flow
 @flow
@@ -43,7 +54,10 @@ def combined_pipeline_flow():
     schema_result = create_schema(wait_for=[extract_result])
 
     # Step 3: Transform and upload text data to BigQuery
-    transform_txt_to_dataframe(wait_for=[schema_result])
+    transform_result = transform_txt_to_dataframe(wait_for=[schema_result])
+
+    # Step 4: Load Parquet data into BigQuery
+    load_to_bigquery(wait_for=[transform_result])
 
 # Run the Prefect flow
 if __name__ == "__main__":
