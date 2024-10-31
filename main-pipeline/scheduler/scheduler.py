4c1b4873696131132994c974e7d029e41cafabd9
victor-floriano <victorgf@bu.edu>
2024-10-31 21:23:02 +0000

    Changed deployment schedule to once a week on Sunday

--- main-pipeline/scheduler/scheduler.py
+++ main-pipeline/scheduler/scheduler.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
         source="https://github.com/Olivia-Peng/BA882-pipeline_G10.git",  
         entrypoint="main-pipeline/flows/etl-flow.py:combined_pipeline_flow",  # Path to flow in the repo
     ).deploy(
-        name="daily-cdc-pipeline",  # deployment name
+        name="weekly-cdc-pipeline",  # deployment name
         work_pool_name="victor-pool1",  # work pool
         job_variables={
             "env": {"ENVIRONMENT": "production"},
             "pip_packages": ["pandas", "requests"]  # Add any other dependencies your flow requires
         },
-        cron="0 23 * * *",  # Schedule: Runs at 11 PM EST
+        cron="0 23 * * 0",  # Schedule: Runs at 11 PM EST on Sundays
         tags=["production"],
         description="The pipeline to extract, transform, and load CDC data into BigQuery",
         version="1.0.0",
