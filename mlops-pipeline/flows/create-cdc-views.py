f338d2bda14b4d233c258f7cc9bff3bbc3e28fd5
victor-floriano <victorgf@bu.edu>
2024-11-08 00:27:30 +0000

    Added prefect flows

---
+++ mlops-pipeline/flows/create-cdc-views.py
@@ -0,0 +1,38 @@
+# CDC Disease Occurrences View Creation Job
+
+# imports
+import requests
+from prefect import flow, task
+from prefect.events import DeploymentEventTrigger
+
+# helper function - generic invoker
+def invoke_gcf(url: str, payload: dict):
+    response = requests.post(url, json=payload)
+    response.raise_for_status()
+    return response.json()
+
+@task(retries=2)
+def create_cdc_views():
+    """Invoke the CDC occurrences Cloud Function to create views and export data to GCS."""
+    url = "https://create-cdc-views-548628906045.us-central1.run.app"  # Replace with the actual URL of your deployed Cloud Function
+    resp = invoke_gcf(url, payload={})
+    return resp
+
+# the flow
+@flow(name="cdc-disease-ml-datasets", log_prints=True)
+def cdc_ml_datasets():
+    # Execute the CDC occurrences view creation
+    create_cdc_views()
+
+# Set up the deployment with an event trigger
+if __name__ == "__main__":
+    # Trigger the cdc_ml_datasets flow when the weekly-cdc-pipeline deployment completes
+    cdc_ml_datasets().deploy(
+        name="cdc-disease-ml-datasets",
+        triggers=[
+            DeploymentEventTrigger(
+                expect={"prefect.flow-run.Completed"},
+                match_related={"prefect.resource.name": "weekly-cdc-pipeline"}
+            )
+        ]
+    )
---
+++ mlops-pipeline/flows/deploy-create-cdc-views.py
@@ -0,0 +1,24 @@
+from prefect import flow
+from prefect.events import DeploymentEventTrigger
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/victor-floriano/ba882-victor-floriano.git@",
+        entrypoint="mlops-pipeline/flows/create-cdc-views.py:cdc_ml_datasets",
+    ).deploy(
+        name="cdc-disease-ml-datasets",
+        work_pool_name="victor-pool1",
+        job_variables={
+            "env": {"ENVIRONMENT": "production"},
+            "pip_packages": ["pandas", "requests"]
+        },
+        tags=["prod"],
+        description="Pipeline to create CDC disease occurrences ML datasets after data extraction to BigQuery.",
+        version="1.0.0",
+        triggers=[
+            DeploymentEventTrigger(
+                expect={"prefect.flow-run.Completed"},
+                match_related={"prefect.resource.name": "weekly-cdc-pipeline"}
+            )
+        ]
+    )
