ac3f155be525adfcd146f9437d8409fa45b85dc3
victor-floriano <victorgf@bu.edu>
2024-12-06 00:10:22 +0000

    Minor fix to library prefect deployment script

--- mlops-pipeline/flows/deploy-sarima-tuning.py
+++ mlops-pipeline/flows/deploy-sarima-tuning.py
@@ -1,20 +1,19 @@
 from prefect import flow
-from prefect.events import DeploymentEventTrigger
-from prefect.orion.schemas.schedules import CronSchedule
 
 if __name__ == "__main__":
+    # Create a deployment from the GitHub repository and specify the entry point
     flow.from_source(
-        source="https://github.com/Olivia-Peng/BA882-pipeline_G10.git",
-        entrypoint="mlops-pipeline/flows/sarima_tuning_flow.py:sarima_tuning_flow",
+        source="https://github.com/Olivia-Peng/BA882-pipeline_G10.git",  
+        entrypoint="mlops-pipeline/flows/sarima-tuning.py:sarima_tuning_flow",  # Path to flow in the repo
     ).deploy(
-        name="sarima-hyperparameter-tuning",
-        work_pool_name="victor-pool1",
+        name="sarima-hyperparameter-tuning",  # Deployment name
+        work_pool_name="victor-pool1",  # Work pool
         job_variables={
             "env": {"ENVIRONMENT": "production"},
-            "pip_packages": ["pandas", "requests", "scikit-learn", "statsmodels", "google-cloud-storage"]
+            "pip_packages": ["pandas", "requests", "scikit-learn", "statsmodels", "google-cloud-storage"]  # Required dependencies
         },
+        cron="0 0 1 */3 *",  # Schedule: Runs on the first day of every 3rd month at midnight UTC
         tags=["prod"],
         description="Run SARIMA hyperparameter tuning for specified disease codes every three months.",
         version="1.0.0",
-        schedule=CronSchedule(cron="0 0 1 */3 *", timezone="UTC")  # First day of every third month
     )
